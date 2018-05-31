9bea03719a3c33e2e986bc8f8400add69879dd13
0xFF <not.0xff@gmail.com>
2018-05-30 21:16:09 -0400

    this will be good code at some point, it just isn't yet

---
+++ Starlet/Core/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Starlet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing Starlet [PurpleEmu]");
+            Console.WriteLine("Version: {0} Build Type: {1}", Core.Settings.Version, Core.Settings.releaseType);
+            if(Core.Settings.releaseType == "Beta")
+            {
+                //warn the poor fucker that all hell is about to break loose
+                Console.WriteLine("It appears that you are running a beta version!");
+                Console.WriteLine("A lot of stuff will probably break, or be broken at the end of this.");
+                Console.WriteLine("Bug reports welcome at {0}", Core.Settings.gitUrl);
+            }
+        }
+    }
+}
---
+++ Starlet/Core/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Starlet.Core
+{
+    public class Settings
+    {
+        public static int Version = 1;
+        public static string releaseType = "Beta";
+        public static string gitUrl = "https://github.com/not0xFF/PurpleEmu";
+    }
+}
